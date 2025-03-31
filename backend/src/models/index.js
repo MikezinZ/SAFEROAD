0c0fecf3a6ee3fcc57012719ad1560c62bee048a
Miguel Henrique <miguelhenriquemelo17@gmail.com>
2025-03-31 12:19:58 -0300

    SafeRoad 0.0.4
    Correção de erros de sintaxe e testes de funcionamento

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -4,7 +4,7 @@ const sequelize = require('../config/database');
 const User = require('./user');
 
 // Importação do modelo de usuário.
-Module.exports = {
+module.exports = {
     sequelize,
     User
 };
