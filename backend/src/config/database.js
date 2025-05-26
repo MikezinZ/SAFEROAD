340335351660625254627f5db35eedad9fcf58e4
Miguel Henrique <miguelhenriquemelo17@gmail.com>
2025-05-26 12:59:25 -0300

    SAFEROAD 0.2.2.2
    Testes de conexão com render

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+const dbUrl = process.env.DATABASE_URL;
+
+if (!dbUrl) {
+    const errorMessage = "ERRO CRÍTICO: DATABASE_URL não está definida nas variáveis de ambiente! O servidor não pode iniciar sem ela.";
+    console.error(errorMessage);
+    throw new Error(errorMessage);
+}
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
