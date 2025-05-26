398bf40e6e71e3df4e58d46ebcd1d156808ea6aa
Miguel Henrique <miguelhenriquemelo17@gmail.com>
2025-05-26 12:37:52 -0300

    SAFEROAD 0.2.1
    Conexão do projeto em um banco Online postgree no Neon(Inutilização do arquivo database.sqlite) e ajustes no backend para suprir as novas necessidades.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const path = require('path');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: path.join(__dirname, '../../database.sqlite'),
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     logging: false
 });
 
