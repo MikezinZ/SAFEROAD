9dee4cadce1b12266953c0d5afb8bd0d8d5f0e46
Miguel Henrique <miguelhenriquemelo17@gmail.com>
2025-05-26 20:31:49 -0300

    SAFEROAD 0.2.2.3
    Alteração no Cors para adição da Url do frontend no arquivo Index.js do Backend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware para permitir requisições de diferentes origens
-app.use(cors());
+const frontendURL = "https://saferoad-lime.vercel.app/"
+app.use(cors({ origin: frontendURL }));
 app.use(express.json());
 
 // configuração do swagger
