448ef724a2aaeec20d327ee553d7d2ffeaa58ee8
Miguel Henrique <miguelhenriquemelo17@gmail.com>
2025-05-27 20:14:06 -0300

    SAFEROAD 0.2.4
    Ajustes mínimos para configuração com Rds da amazon

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -11,7 +11,6 @@ if (!dbUrl) {
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    protocol: 'postgres',
     dialectOptions: {
         ssl: {
             require: true,
