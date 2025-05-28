1375f2d01c0047c454da55177eca484ab2826b51
Miguel Henrique <miguelhenriquemelo17@gmail.com>
2025-05-27 22:15:23 -0300

    SAFEROAD 0.2.5
    preparação do backend para lambda

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 // importaçãoa de módulos
 const express = require('express');
 const cors = require('cors');
+const serverless = require('serverless-http');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const userRoutes = require('./routes/userRoutes');
@@ -14,8 +15,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware para permitir requisições de diferentes origens
-const frontendURL = "https://saferoad-lime.vercel.app"
-app.use(cors({ origin: frontendURL }));
+// const frontendURL = "https://saferoad-lime.vercel.app"
+// app.use(cors({ origin: frontendURL }));
+app.use(cors());
 app.use(express.json());
 
 // configuração do swagger
@@ -62,25 +64,32 @@ app.use('/api/auth', authRoutes);
 
 // tratamento de erro global
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Algo deu errado!');
+    console.error("ERRO GLOBAL", err.stack);
+    res.status(500).json({ message: 'Algo deu errado no servidor.', error: err.message });
 });
 
 // enedpoint de saúde
 app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'OK' });
+    res.status(200).json({ status: 'OK', message: 'Backend SAFEROAD está saudável!' });
 });
 
 // sincronização do banco de dados e inicialização do servidor
 sequelize.sync()
     .then(() => {
-        app.listen(PORT, () => {
-            console.log(`🔥 Servidor rodando na porta ${PORT}`);
-            console.log(`📄 Documentação: http://localhost:${PORT}/api-docs`);
-        });
+        console.log('Sincronização com o banco de dados (RDS) bem-sucedida.');
+        if (process.env.NODE_ENV !== 'production_lambda') {
+            app.listen(PORT, () => {
+                console.log(`🔥 Servidor LOCAL rodando na porta ${PORT}`);
+                console.log(`📄 Documentação LOCAL: http://localhost:${PORT}/api-docs`);
+            });
+        }
     })
     .catch(err => {
         console.error('Falha ao sincronizar com o banco de dados:', err);
+
+        if (process.env.NODE_ENV === 'production_lambda') {
+            throw err;
+        }
     });
 
-module.exports = app; 
+module.exports.handler = serverless(app); 
